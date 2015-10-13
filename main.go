2b30b33ad207c93dca59c357faf6eed956db65d5
Swapnil Agarwal <swapnilagarwal@live.com>
2015-10-13 23:32:53 +0530

    Setup Basic Authentication

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	auth "github.com/abbot/go-http-auth"
+	"net/http"
+	"os"
+)
+
+func Secret(user, realm string) string {
+	if user == os.Getenv("login") {
+		return os.Getenv("password")
+	}
+	return ""
+}
+
+func handle(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
+	fmt.Fprintf(w, "Welcome, %s!", r.Username)
+}
+
+func main() {
+	authenticator := auth.NewBasicAuthenticator("aws-remote.herokuapp.com", Secret)
+	http.HandleFunc("/", authenticator.Wrap(handle))
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
